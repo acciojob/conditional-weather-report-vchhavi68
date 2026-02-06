94a35bce87b8c440ee00e20d403fd8cb3b9ceb74
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 18:44:37 +0530

    Update WeatherDisplay.js

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,1 +1,18 @@
 
+import React from "react";
+
+const WeatherDisplay = ({ weatherData }) => {
+  return (
+    <div>
+      <p>
+        Temperature:{" "}
+        <span style={{ color: weatherData.temperature > 20 ? "red" : "blue" }}>
+          {weatherData.temperature}
+        </span>
+      </p>
+      <p>Conditions: {weatherData.conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
